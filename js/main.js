bc36e966424f01c59684b5b9c25ccfbe34f16263
pavel-kosach <kosachdev@gmail.com>
2022-11-03 21:46:47 +0300

    add: mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector (".navbar");
 const logo = document.querySelector(".logo-svg use");
+const menu = document.querySelector(".mobile-menu");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1){
     navbar.classList.add("navbar-light");
@@ -8,4 +10,8 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove("navbar-light");
     logo.href.baseVal = "img/sprites.svg#logo-light";
   }
+})
+mMenuToggle.addEventListener("click",(event) => {
+  event.preventDefault();
+  menu.classList.toggle("is_open");
 })
