b56221b8077d92bce7bb19c4f11007ef6ec8e179
pavel-kosach <kosachdev@gmail.com>
2022-11-03 23:31:47 +0300

    add: mobile-menu-open

--- js/main.js
+++ js/main.js
@@ -2,16 +2,32 @@ const navbar = document.querySelector (".navbar");
 const logo = document.querySelector(".logo-svg use");
 const menu = document.querySelector(".mobile-menu");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light"); // перекрашивание меню
+  logo.href.baseVal = "img/sprites.svg#logo"; // перекрашивание меню
+};
+const lightModeOff= (event) => {
+  navbar.classList.remove("navbar-light"); // перекрашивание меню
+  logo.href.baseVal = "img/sprites.svg#logo-light"; // перекрашивание меню
+};
+const openMenu = (event) => { // функция открывания меню
+  menu.classList.add("is_open"); // добавляет класс is-open
+  document.body.style.overflow="hidden"; // убирает прокрутку сайта под меню
+  lightModeOn(); // перекрашивание меню
+  mMenuToggle.classList.add("close-menu"); // добавляет класс для стилизации кнопки закрытия меню
+};
+const closeMenu = (event) => { // функция закрытвает меню
+  menu.classList.remove("is_open"); // убирает класс is-open
+  document.body.style.overflow=""; // возвращает прокрутку сайта под меню
+  lightModeOff(); // перекрашивание меню
+  mMenuToggle.classList.remove("close-menu"); // убирает класс для стилизации кнопки закрытия меню
+};
+// изменение цвета navbar при скролле
 window.addEventListener('scroll', () => {
-  if (this.scrollY > 1){
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprites.svg#logo";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprites.svg#logo-light";
-  }
-})
+  this.scrollY > 1 ? lightModeOn() : lightModeOff(); // перекрашивание мен
+});
+// открытие и закрытие мобильного меню
 mMenuToggle.addEventListener("click",(event) => {
   event.preventDefault();
-  menu.classList.toggle("is_open");
-})
+  menu.classList.contains('is_open') ? closeMenu() : openMenu();
+});
