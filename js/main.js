3b06c7078a6c1899acdebcae67aa17ba45e92c6e
pavel-kosach <kosachdev@gmail.com>
2022-11-07 14:12:53 +0300

    add: work plan slider

--- js/main.js
+++ js/main.js
@@ -37,7 +37,31 @@ mMenuToggle.addEventListener("click",(event) => {
   menu.classList.contains('is_open') ? closeMenu() : openMenu();
 });
 
-const swiper = new Swiper('.swiper', { // инициализация слайдера
+
+const swiperSteps = new Swiper(".steps-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.steps-button-next',
+    prevEl: '.steps-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const swiper = new Swiper(".features-slider", { // инициализация слайдера
   speed: 400,
   slidesPerView: 1,
   navigation: {
